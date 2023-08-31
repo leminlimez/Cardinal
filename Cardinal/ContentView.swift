42808b64625b4794a41d6e5b49ff5ce6b7f968b4
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2023-08-30 23:29:42 -0400

    Initial Commit

---
+++ Cardinal/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Cardinal
+//
+//  Created by lemin on 8/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
