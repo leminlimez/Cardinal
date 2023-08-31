9fb04628f71ff26debad48ad382ea301b28d33ef
leminlimez <jirizarry317@gmail.com>
2023-08-30 23:29:42 -0400

    Initial Commit

---
+++ Cardinal/CardinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardinalApp.swift
+//  Cardinal
+//
+//  Created by lemin on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct CardinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
